b8ac6d1b8e72b83c16802d027b1fa11acff689f0
Your Name <you@example.com>
2019-05-24 20:52:35 +0000

    Reorder dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject spike-memory "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [frp "0.1.3"]
                  [cljs-node-io "1.1.2"]
                  [cljsjs/mousetrap "1.6.2-0"]
+                 [frp "0.1.3"]
                  [reagent "0.8.1"]]
   :plugins [[lein-ancient "0.6.15"]]
   :source-paths ["src/helpers"]
