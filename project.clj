6097f4bf68b3903544e204f16b2afddc7290c54f
Your Name <you@example.com>
2019-05-24 07:14:20 +0000

    Define app-view

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
                  [org.clojure/clojurescript "1.10.520"]
                  [frp "0.1.3"]
                  [cljs-node-io "1.1.2"]
+                 [cljsjs/mousetrap "1.6.2-0"]
                  [reagent "0.8.1"]]
   :plugins [[lein-ancient "0.6.15"]]
   :source-paths ["src/helpers"]
--- src/renderer/spike_memory/core.cljs
+++ src/renderer/spike_memory/core.cljs
@@ -1,1 +1,40 @@
-(ns spike-memory.core)
+(ns spike-memory.core
+  (:require [aid.core :as aid]
+            [cats.core :as m]
+            [frp.core :as frp]
+            [reagent.core :as r]))
+
+(frp/defe cancel save edit typing)
+
+(def review
+  (->> edit
+       (aid/<$ false)
+       (m/<> (aid/<$ true (m/<> cancel save)))
+       (frp/stepper true)))
+
+(def edit-component
+  [:form
+   [:textarea {:on-change #(-> %
+                               .-target.value
+                               typing)}]
+   [:button {:on-click #(cancel)} "Cancel"]
+   [:button {:on-click #(save)} "Save"]])
+
+(def review-view
+  [:div {:on-double-click #(edit)
+         :style           {:height   "100%"
+                           :overflow "hidden"
+                           :width    "150px"}}])
+
+(def app-component
+  #(if %
+     review-view
+     edit-component))
+
+(def app-view
+  (m/<$> app-component review))
+
+(frp/run (partial (aid/flip r/render) (js/document.getElementById "app"))
+         app-view)
+
+(frp/activate)
