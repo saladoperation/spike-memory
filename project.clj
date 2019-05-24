e9eeb5becee1827f1381cdb057344682e640e20a
Your Name <you@example.com>
2019-05-24 05:53:57 +0000

    Add frp

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject spike-memory "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [aid "0.1.2"]
-                 [cljs-node-io "1.1.2"]]
+                 [frp "0.1.3"]
+                 [cljs-node-io "1.1.2"]
+                 [reagent "0.8.1"]]
   :plugins [[lein-ancient "0.6.15"]]
   :source-paths ["src/helpers"]
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
